cab3eeec147a960f805587ce90991894bb7315f9
gdutchak <galyna.dutchak99@gmail.com>
2022-10-02 16:00:28 +0300

    bagfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { FormContacts } from './FormContacts/FormContacs';
 import { Filter } from "./Filter/Filter";
 import { ListContacts } from "./ListContacts/ListContacts";
